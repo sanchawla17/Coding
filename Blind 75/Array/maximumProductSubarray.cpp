e12fe303e13cd075c77440064607ec3c6a79c057
Sanyam Chawla <91560018+sanchawla17@users.noreply.github.com>
2023-12-08 15:15:54 +0530

    Create maximumProductSubarray.cpp

---
+++ Blind 75/Array/maximumProductSubarray.cpp
@@ -0,0 +1,17 @@
+//08.12.23
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suff=1,ans=INT_MIN;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(pre==0) pre=1;
+            if(suff==0) suff=1;
+
+            pre*=nums[i];
+            suff*=nums[n-i-1];
+            ans=max(ans,max(pre,suff));
+        }
+        return ans;
+    }
+};
